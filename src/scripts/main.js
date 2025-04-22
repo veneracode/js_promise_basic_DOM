d1abb5accdc11ec98bf2fe99d329def4c3e7eeba
Ivan <tushchukivan@gmail.com>
2025-04-22 21:16:16 +0300

    s

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
+const promise1 = new Promise (resolve => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve()
+  })
+})
+
+const promise2 = new Promise ((_, reject) => {
+  setTimeout(() => {
+    reject()
+  }, 3000)
+})
+
+const newMessage (text, isErorr = false) => {
+  const newMessage = document.createElement('div');
+  newMessage.classList = ('message');
+  if (isErorr) {
+    newMessage.classList = ('error-message')
+  }
+    newMessage.textContent = text;
+    document.body.appendChild(newMessage);
+
+
+  }
+
 
 // write your code here
